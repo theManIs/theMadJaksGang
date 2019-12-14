800f43cf602cf2b692df7d8977a705e6c6c7a510
Jim Raynor <lostlose@yandex.ru>
2019-12-14 18:11:49 +0300

    highlight frame

---
+++ GamePrimal/Deprecated/UserInterfaceBar/WindowCharacterPortrait.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// -6.04 -294.71 22.26
+public class WindowCharacterPortrait : MonoBehaviour
+{
+    public Transform CameraTransform;
+    public Transform FallowTransform;
+    public float RequiredDistance;
+
+    void Update()
+    {
+        //CameraTransform.position = FallowTransform.position + new Vector3(RequiredDistance, RequiredDistance, RequiredDistance);
+    }
+}
---
+++ GamePrimal/Navigation/HighlightFrame/HighlightFrame.cs
@@ -0,0 +1,85 @@
+﻿using Assets.TeamProjects.GamePrimal.Helpers;
+using UnityEngine;
+
+namespace Assets.TeamProjects.GamePrimal.Navigation.HighlightFrame
+{
+    public class HighlightFrame
+    {
+        public GameObject fhgm;
+        public Transform GameObjectToHighlight;
+
+        public bool Engaged = true;
+        private MeshRenderer m_FrameMeshRenderer;
+        private GameObject m_LocalInstance;
+        private SpriteRenderer m_LocalSpriteRenderer;
+
+        private readonly string _frame = ResourcesList.FrameHighlighter;
+
+        // Start is called before the first frame update
+        public void Start()
+        {
+            fhgm = Resources.Load<GameObject>(_frame);
+            m_LocalInstance = Object.Instantiate(fhgm, Vector3.one, fhgm.transform.rotation);
+            m_LocalSpriteRenderer = m_LocalInstance.GetComponent<SpriteRenderer>();
+        }
+
+//        public void FixedUpdate()
+//        {
+//            if (!Engaged) return;
+//
+//            TraceMousePosition();
+//        }
+
+        public void FixedUpdate(Transform objToHighlight)
+        {
+            if (!Engaged) return;
+
+            if (objToHighlight is null) {
+                RemoveHighlight();
+            } else {
+                GameObjectToHighlight = objToHighlight;
+
+                HighlightTheTarget(objToHighlight.gameObject);
+            }
+
+            
+        }
+    
+        private void HighlightTheTarget(GameObject targetGameObject)
+        {
+            m_FrameMeshRenderer = targetGameObject.GetComponent<MeshRenderer>();
+            Vector3 pos = targetGameObject.transform.position;
+            
+            //pos.x = m_FrameMeshRenderer.bounds.center.x;
+            pos.y = m_FrameMeshRenderer.bounds.min.y + m_FrameMeshRenderer.bounds.size.y * .2f;
+            //pos.z = m_FrameMeshRenderer.bounds.center.z;
+
+            m_LocalInstance.transform.position = pos;
+            m_LocalSpriteRenderer.enabled = true;
+        }
+
+        /// <summary>
+        /// <Deprecated>Deprecated</Deprecated>
+        /// </summary>
+//        private void TraceMousePosition()
+//        {
+//            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//
+//            RaycastHit hit;
+//
+//            if (Physics.Raycast(ray, out hit, 100))
+//            {
+//                if (hit.collider.tag == "Targetable")
+//                    HighlightTheTarget(hit.collider.gameObject);
+//                else
+//                    RemoveHighlight();
+//            }
+//        }
+
+        private void RemoveHighlight()
+        {
+            if (m_LocalSpriteRenderer)
+                m_LocalSpriteRenderer.enabled = false;
+        }
+    }
+}

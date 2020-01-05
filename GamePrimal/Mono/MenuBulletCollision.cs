eb6f6556974eaaddfe95b15fbda78c3d1c6198c4
Jim Raynor <lostlose@yandex.ru>
2020-01-05 20:27:33 +0300

    MenuBulletCollision

---
+++ GamePrimal/Mono/MenuBulletCollision.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuBulletCollision : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Color ColorInitial = Color.white;
+    public Color ColorToward = Color.gray;
+
+    private TextMeshProUGUI _tmp;
+    private RectTransform _rt;
+
+    private void Start()
+    {
+        _tmp = GetComponent<TextMeshProUGUI>();
+        _rt = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _tmp.color = ColorToward;
+       _rt.localScale = _rt.localScale * 1.2f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _tmp.color = ColorInitial;
+        _rt.localScale = Vector3.one;
+    }
+}

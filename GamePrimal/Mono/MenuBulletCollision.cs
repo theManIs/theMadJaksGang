1bf327e40cb9469e2facef253962fe0a8148098d
Jim Raynor <lostlose@yandex.ru>
2020-01-06 01:19:05 +0300

    coins; table; dagger; candles

--- GamePrimal/Mono/MenuBulletCollision.cs
+++ GamePrimal/Mono/MenuBulletCollision.cs
@@ -6,25 +6,32 @@ public class MenuBulletCollision : MonoBehaviour, IPointerEnterHandler, IPointer
 {
     public Color ColorInitial = Color.white;
     public Color ColorToward = Color.gray;
+    public float Position;
 
     private TextMeshProUGUI _tmp;
     private RectTransform _rt;
+    private Vector3 _initialScale;
+
+    public bool PickRightNow { get; private set; } = false;
 
     private void Start()
     {
         _tmp = GetComponent<TextMeshProUGUI>();
         _rt = GetComponent<RectTransform>();
+        _initialScale = transform.localScale;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         _tmp.color = ColorToward;
        _rt.localScale = _rt.localScale * 1.2f;
+       PickRightNow = true;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         _tmp.color = ColorInitial;
-        _rt.localScale = Vector3.one;
+        _rt.localScale = _initialScale;
+        PickRightNow = true;
     }
 }
---
+++ GamePrimal/Mono/VelocityObjectTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityObjectTrigger : MonoBehaviour
+{
+    private List<MenuBulletCollision> _mbc;
+    private Vector3 _initPos;
+    private bool _onLoose = false;
+    private Quaternion _possibleRot;
+    private CandleLightHolder[] _candleHolder;
+
+    void Start()
+    {
+        _mbc = new List<MenuBulletCollision>(FindObjectsOfType<MenuBulletCollision>());
+        _initPos = transform.position;
+        Vector3 eulerAngles = transform.rotation.eulerAngles;
+        eulerAngles.y += 90;
+        _possibleRot = Quaternion.Euler(eulerAngles);
+        _candleHolder = FindObjectsOfType<CandleLightHolder>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !_onLoose)
+            foreach (MenuBulletCollision mbcPublic in _mbc)
+                if (mbcPublic.PickRightNow)
+                {
+                    gameObject.AddComponent<Rigidbody>();
+
+                    _initPos.z += mbcPublic.Position;
+                    transform.position = _initPos;
+                    _onLoose = true;
+                }
+
+        if (_onLoose)
+            transform.rotation = Quaternion.Lerp(transform.rotation, _possibleRot, Time.deltaTime * 3);
+    }
+
+    private void OnTriggerEnter()
+    {
+        Destroy(gameObject.GetComponent<Rigidbody>());
+        Destroy(gameObject.GetComponent<VelocityObjectTrigger>());
+
+        foreach (CandleLightHolder candle in _candleHolder) 
+            candle.gameObject.SetActive(false);
+    }
+}

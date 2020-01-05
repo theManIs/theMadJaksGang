41656e80e0aa1eaff2d656cdfeaab0d0ef1bd266
Jim Raynor <lostlose@yandex.ru>
2020-01-05 18:34:04 +0300

    Main UI menu old one

---
+++ GamePrimal/PanelUI/HealthBinder.cs
@@ -0,0 +1,28 @@
+﻿using Assets.GamePrimal.Mono;
+using Assets.TeamProjects.GamePrimal.MainScene;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.PanelUI
+{
+    public class HealthBinder : MonoBehaviour
+    {
+        private Slider _healthSlider;
+        private MainScene _ms;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _healthSlider = GetComponent<Slider>();
+            _ms = FindObjectOfType<MainScene>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_ms.GetFocus())
+                if (_ms.GetFocus().GetComponent<MonoAmplifierRpg>())
+                    _healthSlider.value = _ms.GetFocus().GetComponent<MonoAmplifierRpg>().ViewHealth();
+        }
+    }
+}

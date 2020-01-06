e0706f52cd31ddad0a25d7ad139476376ea8f1be
Jim Raynor <lostlose@yandex.ru>
2020-01-06 21:27:37 +0300

    crossbow shoots in the right direction

--- DemoAnimationScene/MiscellaneousWeapons/CommonScripts/WeaponOperator.cs
+++ DemoAnimationScene/MiscellaneousWeapons/CommonScripts/WeaponOperator.cs
@@ -17,31 +17,18 @@ namespace Assets.TeamProjects.DemoAnimationScene.MiscellaneousWeapons.CommonScri
         public WeaponTypes WeaponType = WeaponTypes.Crossbow;
         public bool isRanged;
         public Transform SpawnPoint;
+        public int ShootPower = 30;
 
-//        public void ShootAnyProjectile(Transform projectile, Transform enemy)
-//        {
-////            Vector3 estimatedPos = enemy.position - transform.position;
-//            GameObject projectileInstance = Instantiate(projectile.gameObject, SpawnPoint.position, projectile.transform.rotation);
-//            Quaternion towardEnemy = Quaternion.FromToRotation(projectileInstance.transform.position, enemy.position);
-////            projectileInstance.transform.LookAt(enemy);
-//            projectileInstance.transform.rotation = towardEnemy;
-//
-//            if (!projectileInstance.GetComponent<Rigidbody>())
-//                projectileInstance.AddComponent<Rigidbody>();
-//
-////            projectileInstance.GetComponent<Rigidbody>().AddRelativeForce(projectileInstance.transform.forward * 3000);
-////            projectileInstance.GetComponent<Rigidbody>().velocity = Vector3.forward * 2;
-//            projectileInstance.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * 1000);
-//
-////            EditorApplication.isPaused = true;
-////            Invoke("Stop", 0.1f);
-//
-//            //            Destroy(projectileInstance, 5f);
-//        }
-//
-//        public void Stop()
-//        {
+        public void ShootAnyProjectile(Transform projectile, Transform enemy)
+        {
+            transform.LookAt(enemy);
+
+            GameObject newProjectile = Instantiate(projectile.gameObject);
+            Rigidbody rb = newProjectile.GetComponent<Rigidbody>();
+            newProjectile.transform.position = SpawnPoint.position;
+            newProjectile.transform.rotation = SpawnPoint.rotation;
+            rb.velocity = newProjectile.transform.forward * ShootPower;
 //            EditorApplication.isPaused = true;
-//        }
+        }
     }
 }
